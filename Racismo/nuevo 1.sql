d1dc5ea6e68bfe3fc15545c8799fb97405caa971
terrok9 <jf.barrera10@uniandes.edu.co>
2022-03-22 02:32:15 -0500

    update marzo 22

---
+++ Racismo/nuevo 1.sql
@@ -0,0 +1,31 @@
+(
+	/* Palabras relacionadas con racismo (denuncia de racismo)*/
+	racismo OR racista OR racistas OR racializado OR racializada OR hashtag:#racismo OR hashtag:#racista
+	/* Palabras relacionadas con migración */
+	OR migrante OR inmigrante OR migrantes OR inmigrantes OR migración
+	/* Palabras relacionadas con racismo */
+	OR "color de piel" OR "color cartón" OR "color mole" OR "color cazuela" OR prieto OR prieta
+	OR prietos OR prietas OR prietx OR prietxs OR clasista OR discriminación OR acomplejado
+	OR acomplejados OR acomplejada OR acomplejadas OR resentido OR resentidos OR inferioridad
+	OR clasismo OR "clase social" OR indigenas OR indio OR clasistas
+	OR ((raza OR raza) AND (humano OR humanas OR humanos OR discriminación OR discriminacion))
+	
+	NOT (
+		/*Gobierno, agrupaciones políticas, contenido sexual */
+		amlo OR "andres manuel" OR hashtag:#morena OR Mentions:@GobiernoMX OR Mentions:@SEGOB_mx OR 
+		Mentions:@Mx_Diputados OR User:@Presidencia_HN OR "Adolfo Prieto" OR presidente OR PRI OR PAN 
+		OR "cerro prieto" OR User:@PartidoMorenaMx OR Mentions:@PartidoMorenaMx OR ToUser:@lopezobrador_
+		OR "Guillermo prieto" OR "agua prieta" OR verga OR venuda OR panocha
+		)
+)
+
+/* Themes schemas*/
+/* Género */
+ANY mujer, mujeres, trans, transexual, feminista, feminazi, transexuales, vieja, chava, morra, misógino
+	, misoginía, acosada, "acoso sexual", violada
+/* Migración */
+ANY migrante, invasores, invasor, caravana, extranjero, extranjeros, extranjera, extranjeras, inmigrantes,
+	migrantes, hondureño, sudaca, haitiano, dominicano, mico, chango, changos, micos, inmigrante, xenofobia,
+	xenófobo
+/* Tono de piel */
+ANY negrito, "piel mugre", "color mole", "color carton", "tez humilde", "piel sucia", percudio, percudida
